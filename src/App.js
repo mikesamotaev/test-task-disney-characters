6362fa5075461454e57b81b89214a0231bbb1f2f
mikesamotaev <samotaev.mikhail@gmail.com>
2022-06-28 14:25:57 +0300

    Filtering of characters caused by onChange search input

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import { useEffect, useState } from "react"
 
 const App = () => {
+  // State for all characters
   const [characters, setCharacters] = useState([])
 
+  // State for filtered characters
+  const [searchText, setSearchText] = useState('')
+  const [filteredCharacters, setFilteredCharacters] = useState([])
+
+  // Initial fetch of all characters
   useEffect(() => {
     fetch('https://api.disneyapi.dev/characters')
       .then(response => response.json())
       .then(dataObj => setCharacters(dataObj.data))
   }, [])
 
+  // Filtering of characters caused by onChange
+  const handleChangeSearchText = (e) => {
+    const input = e.target.value
+    setSearchText(input)
+
+    if (input === '') {
+      setFilteredCharacters([])
+    } else {
+      setFilteredCharacters(characters.filter((item) => {
+        return item.name.toLowerCase().indexOf(input.toLowerCase()) !== -1
+      }))
+    }
+  }
+
+  // Render characters (all or filtered)
   const renderCharactersList = () => {
     if (characters.length === 0) return null
 
+    const charactersForRender = (searchText !== '') ? filteredCharacters : characters
+
     return (
       <div className="characters__list">
-        {characters.map((item) => {
+        {charactersForRender.map((item) => {
           return (
             <div className="characters__item" key={item._id}>
             <img className="characters__item-img" src={item.imageUrl} alt={item.name} />
@@ -26,9 +49,18 @@ const App = () => {
     )
   }
 
+  // Render the app
   return (
     <div className="container">
       <h1 className="characters__title">Disney API characters</h1> 
+      <form className="characters__form">
+        <input 
+          className="characters__form-input" 
+          type="text" 
+          placeholder="Search character"
+          value={searchText}
+          onChange={handleChangeSearchText} />
+      </form>
       {renderCharactersList()}
     </div>
   )
