ca949452ede2f26466e52780425c75da80c54f59
mikesamotaev <samotaev.mikhail@gmail.com>
2022-06-28 16:15:49 +0300

    Toggle new field favourite after click

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import classnames from "classnames";
 
 const App = () => {
   // State for all characters
@@ -8,11 +9,13 @@ const App = () => {
   const [searchText, setSearchText] = useState('')
   const [filteredCharacters, setFilteredCharacters] = useState([])
 
-  // Initial fetch of all characters
+  // Initial fetch of all characters and adding field favourite
   useEffect(() => {
     fetch('https://api.disneyapi.dev/characters')
       .then(response => response.json())
-      .then(dataObj => setCharacters(dataObj.data))
+      .then(dataObj => setCharacters(dataObj.data.map((item) => { 
+        return {...item, 'favourite': false} 
+      })))
   }, [])
 
   // Filtering of characters caused by onChange
@@ -29,6 +32,18 @@ const App = () => {
     }
   }
 
+  // Favourites adding/deleting caused by icon onClick
+  const handleChangeFavourite = (id) => {
+    const newCharacters = characters.map((item) => {
+      if (item._id === id) {
+        item.favourite = !item.favourite
+      }
+      return item
+    })
+
+    setCharacters(newCharacters)
+  }
+
   // Render characters (all or filtered)
   const renderCharactersList = () => {
     if (characters.length === 0) return null
@@ -38,9 +53,18 @@ const App = () => {
     return (
       <div className="characters__list">
         {charactersForRender.map((item) => {
+          const favClasses = {
+            "characters__item-fav": true,
+            "added": item.favourite
+          }
+
           return (
             <div className="characters__item" key={item._id}>
-            <img className="characters__item-img" src={item.imageUrl} alt={item.name} />
+              <div 
+                className={classnames(favClasses)}
+                onClick={() => {handleChangeFavourite(item._id)}}
+                ></div>
+              <img className="characters__item-img" src={item.imageUrl} alt={item.name} />
               <div className="characters__item-name">{item.name}</div>
             </div>
           )
@@ -60,6 +84,7 @@ const App = () => {
           placeholder="Search character"
           value={searchText}
           onChange={handleChangeSearchText} />
+          <input type="checkbox" />Show only favourites
       </form>
       {renderCharactersList()}
     </div>
